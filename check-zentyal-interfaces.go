b66206f0cfc21264d29048834b3335d215bd88e0
Eny Rueda <edrueda@4geeks.co>
2016-08-25 09:13:12 -0400

    pruebas .env

--- check-zentyal-interfaces.go
+++ check-zentyal-interfaces.go
@@ -8,6 +8,8 @@ import (
     "os"
     "time"
     "net/smtp"
+    "github.com/ashwanthkumar/slack-go-webhook"
+    "github.com/joho/godotenv"
 )
 
 func check(e error) {
@@ -17,25 +19,46 @@ func check(e error) {
 }
 
 func send(text string) {
+    fmt.Println("enviando correo")
     auth := smtp.PlainAuth(
         "",
-        "<USER>",
-        "<PASSWORD>",
-        "<HOST>",
+        os.Getenv("EMAIL_USER"),
+        os.Getenv("EMAIL_PASSWORD"),
+        os.Getenv("EMAIL_HOST"),
     )
-    
+    fmt.Println(text)
     err := smtp.SendMail(
-        "<HOST:PORT>",
+        os.Getenv("EMAIL_HOST") + ":" + os.Getenv("EMAIL_PORT"),
         auth,
-        "<FROM>",
-        []string{"<RCPT>"},
+        os.Getenv("EMAIL_TO"),
+        []string{os.Getenv("EMAIL_FROMT")},
         []byte(text),
     )
-
+    fmt.Println("correo enviado")
     check(err)
 }
 
+func sendSlack(text string) {
+    webhookUrl := os.Getenv("SLACK_WEBHOOK")
+    fmt.Println("enviando slack")
+
+    payload := slack.Payload {
+      Text: text,
+      Username: os.Getenv("SLACK_USERNAME"),
+      Channel: os.Getenv("SLACK_CHANNEL"),
+    }
+
+    err := slack.Send(webhookUrl, "", payload)
+
+    fmt.Println("slack enviado")
+    
+    if len(err) > 0 {
+      fmt.Printf("error: %s\n", err)
+    }
+}
+
 func timeFile(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: homePage4")
     inter := r.URL.Query().Get("inter")
 
     if inter == "" && inter != "eth0" && inter != "eth2" {
@@ -47,14 +70,17 @@ func timeFile(w http.ResponseWriter, r *http.Request) {
 
     if err != nil {
         send(err.Error())
+        sendSlack(err.Error())
         fmt.Fprintf(w, "False")
         return
     }
 
     duration := time.Since(info.ModTime())
+    fmt.Println(duration.Minutes())
 
     if duration.Minutes() > 4 {
         send("Error en interfaz " + inter)
+        sendSlack("Error en interfaz " + inter)
         fmt.Fprintf(w, "False")
         return
     }
@@ -64,6 +90,7 @@ func timeFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: homePage3")
     inter := r.URL.Query().Get("inter")
 
     if inter == "" && inter != "eth0" && inter != "eth2" {
@@ -80,11 +107,16 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
+    fmt.Println("Endpoint Hit: homePage2")
     http.HandleFunc("/", homePage)
     http.HandleFunc("/timeFile", timeFile)
-    log.Fatal(http.ListenAndServe("<:PORT>", nil))
+    log.Fatal(http.ListenAndServe(":9001", nil))
 }
 
 func main() {
+	fmt.Println(os.Getenv("SLACK_WEBHOOK"))
+	err := godotenv.Load()
+	check(err)
+    fmt.Println("Endpoint Hit: homePage1")
     handleRequests()
 }
